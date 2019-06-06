67c4c1e193d3d64bac5eb67956a4379530d24a83
Sam Hodak <slhodak@gmail.com>
2019-06-05 23:25:35 -0700

    Implement data generation functions for sql and nosql

--- schema.sql
+++ schema.sql
@@ -4,12 +4,28 @@ USE listings;
 
 CREATE TABLE listings (
     id                INT AUTO_INCREMENT,
-    img               VARCHAR(250),
+    img               INT,
     house_type        VARCHAR(25),
     location          VARCHAR(100),
     description       VARCHAR(100),
     cost_per_night    INT NOT NULL,
     rating            DECIMAL(5,2),
     votes             INT NOT NULL,
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
+    FOREIGN KEY(id) REFERENCES photos(url);
+);
+
+CREATE TABLE listing_relations (
+  id                INT AUTO_INCREMENT,
+  id_a              INT NOT NULL,
+  id_b              INT NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY(id_a) REFERENCES listings(id);
+  FOREIGN KEY(id_b) REFERENCES listings(id;
+);
+
+CREATE TABLE photos (
+  id                INT AUTO_INCREMENT,
+  img               VARCHAR
+  PRIMARY KEY(id),
 );
