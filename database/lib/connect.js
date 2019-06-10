6beea86a6033d55894371c1150150123febfed59
Sam Hodak <slhodak@gmail.com>
2019-06-10 12:22:07 -0700

    Begin test refactor to include neo4j

--- database/lib/connect.js
+++ database/lib/connect.js
@@ -1,10 +1,18 @@
 const sqlite = require('sqlite3');
+const neo4j = require('neo4j-driver');
 const path = require('path');
 
-module.exports = new sqlite.Database(path.resolve(__dirname, '../storage/more_listings.db'), (err, response) => {
+// ## SQLite3
+
+module.exports.sqlite = new sqlite.Database(path.resolve(__dirname, '../storage/more_listings.db'), (err) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(response);
+    console.log('Connected to SQLite database');
   }
 });
+
+// ### Neo4j
+
+// const driver = neo4j.driver(uri, neo4j.auth.basic.user(password));
+// const session = driver.session();
--- database/lib/tests.js
+++ database/lib/tests.js
@@ -1,32 +1,42 @@
 const db = require('./connect');
-//  Send queries to db at variable rate
 
-const queries = {
-  simple: `SELECT * FROM listing WHERE rowid = ?`,
-  full: 'select L.*, P.photo_url from listing L inner join listing_relation LR inner join photo P on ((LR.listing_b = P.listing) and (LR.listing_b = L.rowid)) where LR.listing_a = ?;'
-}
-
-const testSuite = {
+const testSQLite = {
+  queries: {
+    simple: `SELECT * FROM listing WHERE rowid = ?`,
+    full: 'select L.*, P.photo_url from listing L inner join listing_relation LR inner join photo P on ((LR.listing_b = P.listing) and (LR.listing_b = L.rowid)) where LR.listing_a = ?;'
+  },
   getN(n) {
     const start = Date.now();
     console.log('Starting test');
     for (let i = 0; i < n; i++) {
-      testSuite.getRandom(n, i, () => {
+      testSQLite.getRandom(n, i, () => {
         console.log(`Retrieved ${n} in ${(Date.now() - start) / 1000} seconds`);
       });
     }
   },
   getRandom(n, i, callback) {
-    db.all(queries.full, [Math.floor(Math.random() * n)], (err, rows) => {
+    db.sqlite.all(testSQLite.queries.full, [Math.floor(Math.random() * n)], (err, rows) => {
       if (err) {
         console.log(err);
       }
       if (i === n - 1) {
-        // console.log(rows);
+        console.log(rows);
         callback();
       }
     });
   }
 }
 
-testSuite.getN(1000);
+// testSQLite.getN(1000);
+
+const testNeo4j = {
+  queries: '',
+  getN(n) {
+    //  loop produce many queries
+  },
+  getRandom(n, i, callback) {
+    //  send query to neo4j driver
+  }
+}
+
+// testNeo4j.getN(1000);
