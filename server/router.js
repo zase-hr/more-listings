4d98a17409d8880b5e312502cf66965137528a0e
Sam Hodak <slhodak@gmail.com>
2019-06-13 13:52:58 -0700

    Revert "Update error msgs"
    This reverts commit 1f57760a67863c4daf202443022e5c27d74f0d51.

--- server/router.js
+++ server/router.js
@@ -15,7 +15,7 @@ app.use(express.static(`${__dirname}/../public/dist`));
 app.get('/:id/RecommendedListings', (req, res) => {
   db.getRecommendedListings(neo4jDriver, req.params.id, (err, result) => {
     if (err) {
-      res.status(500).send('Error getting recommended listings\n' + err);
+      res.status(500).send(err);
     } else {
       res.status(200).send(result);
     }
@@ -26,7 +26,7 @@ app.get('/:id/RecommendedListings', (req, res) => {
 app.get('/ListingsByDesc', (req, res) => {
   db.getListingsByDescription(neo4jDriver, req.body.description, (err, data) => {
     if (err) {
-      res.status(500).send('Error getting listing by description\n' + err);
+      res.status(500).send();
     } else {
       res.status(200).send(data);
     }
@@ -59,7 +59,7 @@ app.get('/:id/Listing', (req, res) => {
 app.patch('/Listing/:id', (req, res) => {
   db.updateOneListing(neo4jDriver, req.body, req.params.id, (err, result) => {
     if (err) {
-      res.status(500).send('Error updating one listing\n' + err);
+      res.status(500).send();
     } else {
       res.status(200).send(result);
     }
@@ -70,7 +70,7 @@ app.patch('/Listing/:id', (req, res) => {
 app.delete('/Listing/:id', (req, res) => {
   db.deleteOneListing(neo4jDriver, req.params.id, (err, result) => {
     if (err) {
-      res.status(500).send('Error deleting one listing\n' + err);
+      res.status(500).send();
     } else {
       res.status(200).send(result);
     }
