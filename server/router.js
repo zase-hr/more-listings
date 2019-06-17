732065a1f6492e57ef8fecdc8db79c8f4dabdd5a
Sam Hodak <slhodak@gmail.com>
2019-06-17 11:26:29 -0700

    move loaderio to public folder

--- server/router.js
+++ server/router.js
@@ -3,13 +3,10 @@ const express = require('express');
 const path = require('path');
 const compression = require('compression');
 const db = require('../database/models.js');
-const loaderio = require('../loaderio.txt');
 
 const app = express();
 app.use(compression());
 app.use(express.static(`${__dirname}/../public/dist`));
-
-app.use('/loaderio-3747473d2180c64421e92a770eb55e32', express.static(path.resolve(__dirname, '../loaderio/')));
 
 // Get Recommended Listings
 app.get('/MoreHomes', db.getRecommendedListings);
