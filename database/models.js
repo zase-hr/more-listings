00ecce848819898589535bf57840e925ed40662f
Sam Hodak <slhodak@gmail.com>
2019-06-20 16:23:00 -0700

    update model with bolt session subscribing

--- database/models.js
+++ database/models.js
@@ -1,43 +1,50 @@
-// const driver = require('./connect');
-const request = require('request')
+const driver = require('./connect');
+// const request = require('request')
 
 const getRecommendedListings = (req, res) => {
   
   // ## Bolt Protocol Version
-  // let query = `MATCH (a:Listing {id: ${req.query.id}})-[:RECOMMENDS]->(b:Listing) RETURN b`;
-  // const session = driver.session();
-  // session.run(query)
-  //   .then((result) => {
-  //     session.close();
-  //     res.status(200).send(result.records);
-  //   })
-  //   .catch((err) => {
-  //     session.close();
-  //     res.status(500).send(err);
-  //   });
+  let query = `MATCH (a:Listing {id: ${req.query.id}})-[:RECOMMENDS]->(b:Listing) RETURN b`;
+  const session = driver.session();
+  session
+  .run(query)
+  .subscribe({
+    onNext: function(record) {
+      console.log(record.get('id'));
+      // res.status(200).send(body);
+    },
+    onCompleted: function() {
+      res.status(200).send(body);
+      session.close()
+    },
+    onError: function(error) {
+      res.status(500).send(err);
+      console.log(error)
+    }
+  })
 
   // ## HTTP Version
-  request({
-    uri: 'http://localhost:7474/db/data/transaction/commit',
-    headers: {
-      "Accept": "application/json; charset=UTF-8",
-      "Content-Type": "application/json",
-      "Authorization": "Basic bmVvNGo6aG9kYWs="
-    },
-    method: 'POST',
-    body: {
-      "statements" : [ {
-        "statement" : `MATCH (a:Listing {id: ${req.query.id}})-[:RECOMMENDS]->(b:Listing) RETURN b`    
-      } ]
-    },
-    json: true
-  }, (err, response, body) => {
-    if (!err) {
-      res.status(200).send(body);
-    } else {
-      res.status(500).send(err);
-    }
-  });
+  // request({
+  //   uri: 'http://54.71.225.17:7474/db/data/transaction/commit',
+  //   headers: {
+  //     "Accept": "application/json; charset=UTF-8",
+  //     "Content-Type": "application/json",
+  //     "Authorization": "Basic bmVvNGo6aDZ1NCVrcg=="
+  //   },
+  //   method: 'POST',
+  //   body: {
+  //     "statements" : [ {
+  //       "statement" : `MATCH (a:Listing)-[:RECOMMENDS]->(b:Listing) WHERE id(a)=${req.query.id} RETURN b`    
+  //     } ]
+  //   },
+  //   json: true
+  // }, (err, response, body) => {
+  //   if (!err) {
+  //     res.status(200).send(body);
+  //   } else {
+  //     res.status(500).send(err);
+  //   }
+  // });
     
 };
 
--- server/k6TestDatabase.js
+++ server/k6TestDatabase.js
@@ -2,15 +2,17 @@ import k6http from "k6/http";
 import { sleep } from "k6";
 
 export let options = {
-  vus: 10,
-  duration: "30s"
+  stages: [
+    { duration: "10s", target: 300 },
+    { duration: "30s", target: 3000 }
+  ],
 };
 
 export default function() {
-  var url = "http://localhost:7474/db/data/transaction/commit";
+  var url = "http://127.0.0.1:7474/db/data/transaction/commit";
   var payload = JSON.stringify({ 
     statements: [ {
-      statement: "MATCH (a:Listing {id: 99})-[:RECOMMENDS]->(b:Listing) RETURN b"   
+      statement: `MATCH (a:Listing {id: ${Math.ceil(Math.random()*10000000)}})-[:RECOMMENDS]->(b:Listing) RETURN b`   
     } ]
   });
   var headers = {
